d068246dc04e738f1e1bc8cca48a2c3c50e69b18
Zhuldyz <149614840+Assylbekovna@users.noreply.github.com>
2025-12-02 11:52:30 +0600

    Create Product.cs

---
+++ Models/Product.cs
@@ -0,0 +1,16 @@
+namespace OnlineOrderSystem.Models
+{
+    public class Product
+    {
+        public int Id { get; }
+        public string Name { get; }
+        public decimal Price { get; }
+
+        public Product(int id, string name, decimal price)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+        }
+    }
+}
